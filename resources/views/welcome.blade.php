409435612653c9d0ed32e76f0a54a671ec7f3f7b
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2025-12-05 17:10:59 +0700

    Fix welcome page for production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <title>MindTrack</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <!-- Debug: Cek URL yang di-generate -->
+    <!-- URL: {{ asset('css/welcome.css') }} -->
     <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 </head>
 <body>
